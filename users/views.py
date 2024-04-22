93f7072175725cbf15fec472144c60c8e414d629
nouhben <benkadi.nouh@icloud.com>
2024-04-23 00:31:56 +0100

    using custom form

--- users/views.py
+++ users/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 # Create your views here.
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+
+from users.forms import UserSignupForm 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserSignupForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data['username']
             messages.success(request, f'Account was successfully registered for {username}')
             return redirect('blog-home')
@@ -15,7 +18,7 @@ def register(request):
             messages.error(request, f'Account creation failed')
 
     else:
-        form = UserCreationForm()
+        form = UserSignupForm()
     
     return render(request, 'users/register.html', {'form': form})
     
