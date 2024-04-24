fb4c433a2cdadb0dbcb5133460a6ad3567ee5da5
nouhben <benkadi.nouh@icloud.com>
2024-04-24 14:16:19 +0100

    auth system

--- writely/urls.py
+++ writely/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 from django.contrib import admin # type: ignore
 from django.urls import path, include # type: ignore
 from users import views as users_views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # fro my home page
     # path('blog/', include('blog.urls')),
     path('', include('blog.urls')),
-    path('register/', users_views.register, name='register')
+    path('register/', users_views.register, name='register'),
+    # Authentication system views: class based views
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
