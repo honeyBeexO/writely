3e767a0f1cdf4e409ad84b0505bbd3fee4b26a30
nouhben <benkadi.nouh@icloud.com>
2024-04-24 01:15:30 +0100

    catalog app models and default methodes

--- catalog/models.py
+++ catalog/models.py
@@ -23,6 +23,18 @@ class Book(models.Model):
     author =  models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
     genre = models.ForeignKey(Genre, null=True, on_delete=models.SET_NULL)
     language = models.ForeignKey(Language, null=True,on_delete=models.SET_NULL)
+        # Metadata
+    class Meta:
+        ordering = ['-title']
+
+    # Methods
+    def get_absolute_url(self):
+        """Returns the URL to access a particular instance of MyModelName."""
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.my_field_name
 
 class BookInstance(models.Model):
        uniqueId = models.CharField(max_length=12, unique=True)
@@ -31,4 +43,6 @@ class BookInstance(models.Model):
        imprint = models.TextField()
        borrower = models.ForeignKey(User, null=True,on_delete=models.SET_NULL)
        
-       
+class GenreBook(models.Model):
+    genre = models.OneToOneField(Genre, on_delete=models.CASCADE)
+    book = models.OneToOneField(Book, on_delete=models.CASCADE)      
