f658e555e4964d40227de36cc361830cdc4e43fa
nouhben <benkadi.nouh@icloud.com>
2024-04-25 01:24:32 +0100

    admin section & custom views in Catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,12 +8,16 @@ admin.site.register(models.Language)
 #admin.site.register(models.BookInstance)
 
 #admin.site.register(models.Author)
+class BooksInstanceInline(admin.TabularInline):
+    model = models.BookInstance
 @admin.register(models.Book) # this the same as: admin.site.register(models.Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
 @admin.register(models.BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
@@ -25,3 +29,4 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('status', 'due_back')
         }),
     )
+    
