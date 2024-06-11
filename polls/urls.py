d1f2ba77e3366f8227b336fc68c2e6d4f5b491c7
nouhben <benkadi.nouh@icloud.com>
2024-06-11 16:14:37 +0100

    genric views: ListView, DetailView..asview(), context_object_name, template_name

--- polls/urls.py
+++ polls/urls.py
@@ -3,11 +3,15 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('',views.index,name='polls-index'),
+    #path('',views.index,name='polls-index'),
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    #path("<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    #path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    # using the generic views
+    path('',views.IndexView.as_view(),name='index'),
+    path('<int:pk>/',views.DetailView.as_view(),name='detail'),
+    path('<int:pk>/results/',views.ResultView.as_view(),name='results'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render # type: ignore
 
 # Create your views here.
@@ -46,3 +48,24 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/result.html"
+    
+    
