ca5b5ec9bdcf9f09cd43a5115cd89c8b524c92cc
nouhben <benkadi.nouh@icloud.com>
2024-06-20 01:03:54 +0100

    waffel done

--- creamsQuizz/urls.py
+++ creamsQuizz/urls.py
@@ -3,9 +3,14 @@ from . import views
 app_name = 'quizz'
 urlpatterns = [
     path('', views.index, name="index"),
+    #Lists of desserts names
     path('waffels/',views.WaffelListView.as_view(),name='waffels'),
     path('crepes/',views.CrepesListView.as_view(),name='crepes'),
     path('cookies/',views.CookieDoughsListView.as_view(),name='cookies'),
     #path('waffels/<int:pk>/',views.WaffelDetailView.as_view(),name='waffel-detail'),
     path('waffels/<int:waffel_id>/',views.waffel_detail,name='waffel-detail'),
+    path('waffels/<int:waffel_id>/vote/',views.waffel_vote,name='waffel-vote'),
+    path('waffel/<int:pk>/result/', views.WaffelResultView.as_view(), name='waffel-result'),
+
+    
 ]
--- creamsQuizz/views.py
+++ creamsQuizz/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404 # type: ignore
+from django.shortcuts import render, get_object_or_404, redirect # type: ignore
 from .models import Waffel,CookieDough,Crepe,Cake,Sauce,Topping,Scoop
 # Create your views here.
 
@@ -39,81 +39,163 @@ class WaffelDetailView(generic.DetailView):
     template_name = 'creamsQuizz/waffel_detail.html'
     context_object_name = 'dessert'
     
+# def waffel_detail(request, waffel_id):
+#     try:
+#         waffel = Waffel.objects.get(pk=waffel_id)
+#     except Waffel.DoesNotExist:
+#         raise Http404("Waffel not found")
+#     context = {
+#         'waffel': waffel,
+#         'ingredient_categories':{
+#             'toppings':Topping.objects.all(),
+#             'sauces':Sauce.objects.all(),
+#             'cakes':Cake.objects.all(),
+#             'scoops':Scoop.objects.all() 
+#             }, 
+#         }
+#     return render(request, 'creamsQuizz/waffel_detail.html', context=context)
+
+# def waffel_vote(request, waffel_id):
+        
+#     if request.method=='POST':
+#         try:
+#             waffel = get_object_or_404(Waffel, pk=waffel_id)
+#             #selected_toppings = waffel.choice_set.get(pk=request.POST["choice"])
+#             selected_toppings = request.POST.getlist('Toppings')
+#             selected_sauces = request.POST.getlist('Sauces')
+#             selected_cakes = request.POST.getlist('Cakes')
+#             selected_scoops = request.POST.getlist('Scoops')
+#             # Ensure at least one ingredient is selected
+#             print(selected_toppings)
+#             print(selected_sauces)
+#             print(selected_cakes)
+#             print(selected_scoops)
+#             if not (selected_toppings or selected_sauces or selected_cakes or selected_scoops):
+#                 raise ValueError("You must select at least one ingredient.")
+
+#         except (KeyError, ValueError, Waffel.DoesNotExist) as e:
+#             # Redisplay the question voting form.
+#             error_message = str(e)
+#             return render(
+#                 request,
+#                 "creamsQuizz/waffel_detail.html",
+#                 {
+#                     "dessert": waffel,
+#                     "error_message": e,
+#                 },
+#             )
+#         else:
+#             # selected_choice.votes = F("votes") + 1
+#             # selected_choice.save()
+#             # Always return an HttpResponseRedirect after successfully dealing
+#             # with POST data. This prevents data from being posted twice if a
+#             # user hits the Back button.
+#             correct_toppings = waffel.toppings.values_list('id',flat=True)
+#             correct_sauces = waffel.sauces.values_list('id',flat=True)
+#             correct_cakes = waffel.cakes.values_list('id',flat=True)
+#             correct_scoops = waffel.scoops.values_list('id',flat=True)
+#             if(set(correct_toppings) == set(selected_toppings) and 
+#                set(correct_sauces) == set(selected_sauces) and 
+#                set(correct_cakes) == set(correct_cakes) and 
+#                set(correct_scoops) == set(selected_scoops)
+#                ):
+#                 return HttpResponseRedirect(reverse("polls:results", args=(waffel.id,)))
+#             else:
+#                 return render(
+#                 request,
+#                 "creamsQuizz/waffel_detail.html",
+#                 {
+#                     "dessert": waffel,
+#                     "error_message": 'Incorrect Choices',
+#                 },
+#             )
+#     else:
+#         return render(
+#                 request,
+#                 "creamsQuizz/waffel_detail.html",
+#                 {
+#                     "dessert": waffel,
+#                     "error_message": 'Not a proper post request',
+#                 },
+#             )
+    
 def waffel_detail(request, waffel_id):
-    try:
-        dessert = Waffel.objects.get(pk=waffel_id)
-    except Waffel.DoesNotExist:
-        raise Http404("Waffel not found")
+    waffel = get_object_or_404(Waffel, pk=waffel_id)
     context = {
-        'dessert': dessert,
-        'ingredient_categories':{
-            'toppings':Topping.objects.all(),
-            'sauces':Sauce.objects.all(),
-            'cakes':Cake.objects.all(),
-            'scoops':Scoop.objects.all() 
-            }, 
-        }
+        'waffel': waffel,
+        'ingredient_categories': {
+            'toppings': Topping.objects.all(),
+            'sauces': Sauce.objects.all(),
+            'cakes': Cake.objects.all(),
+            'scoops': Scoop.objects.all()
+        },
+    }
     return render(request, 'creamsQuizz/waffel_detail.html', context=context)
 
-def vote(request, waffel_id):
-        
-    if request.method=='POST':
+def waffel_vote(request, waffel_id):
+    if request.method == 'POST':
         try:
             waffel = get_object_or_404(Waffel, pk=waffel_id)
-            #selected_toppings = waffel.choice_set.get(pk=request.POST["choice"])
-            selected_toppings = request.POST.getlist('Toppings')
-            selected_sauces = request.POST.getlist('Sauces')
-            selected_cakes = request.POST.getlist('Cakes')
-            selected_scoops = request.POST.getlist('Scoops')
-            # Ensure at least one ingredient is selected
+
+            selected_toppings = request.POST.getlist('toppings')
+            selected_sauces = request.POST.getlist('sauces')
+            selected_cakes = request.POST.getlist('cakes')
+            selected_scoops = request.POST.getlist('scoops')
+
             if not (selected_toppings or selected_sauces or selected_cakes or selected_scoops):
                 raise ValueError("You must select at least one ingredient.")
 
-        except (KeyError, ValueError, Waffel.DoesNotExist) as e:
-            # Redisplay the question voting form.
+            correct_toppings = list(waffel.toppings.values_list('id', flat=True))
+            correct_sauces = list(waffel.sauces.values_list('id', flat=True))
+            correct_cakes = list(waffel.cakes.values_list('id', flat=True))
+            correct_scoops = list(waffel.scoops.values_list('id', flat=True))
+
+            if (set(map(int, selected_toppings)) == set(correct_toppings) and
+                set(map(int, selected_sauces)) == set(correct_sauces) and
+                set(map(int, selected_cakes)) == set(correct_cakes) and
+                set(map(int, selected_scoops)) == set(correct_scoops)):
+                return redirect(reverse("quizz:waffel-result", args=(waffel.id,)))
+            else:
+                error_message = "Incorrect choices. Please try again."
+                return render(
+                    request,
+                    "creamsQuizz/waffel_detail.html",
+                    {
+                        "waffel": waffel,
+                        "ingredient_categories": {
+                            'toppings': Topping.objects.all(),
+                            'sauces': Sauce.objects.all(),
+                            'cakes': Cake.objects.all(),
+                            'scoops': Scoop.objects.all()
+                        },
+                        "error_message": error_message,
+                    },
+                )
+        except (KeyError, ValueError) as e:
             error_message = str(e)
             return render(
                 request,
                 "creamsQuizz/waffel_detail.html",
                 {
-                    "dessert": waffel,
-                    "error_message": e,
+                    "waffel": waffel,
+                    "ingredient_categories": {
+                        'toppings': Topping.objects.all(),
+                        'sauces': Sauce.objects.all(),
+                        'cakes': Cake.objects.all(),
+                        'scoops': Scoop.objects.all()
+                    },
+                    "error_message": error_message,
                 },
             )
-        else:
-            # selected_choice.votes = F("votes") + 1
-            # selected_choice.save()
-            # Always return an HttpResponseRedirect after successfully dealing
-            # with POST data. This prevents data from being posted twice if a
-            # user hits the Back button.
-            correct_toppings = waffel.toppings.values_list('id',flat=True)
-            correct_sauces = waffel.sauces.values_list('id',flat=True)
-            correct_cakes = waffel.cakes.values_list('id',flat=True)
-            correct_scoops = waffel.scoops.values_list('id',flat=True)
-            if(set(correct_toppings) == set(selected_toppings) and 
-               set(correct_sauces) == set(selected_sauces) and 
-               set(correct_cakes) == set(correct_cakes) and 
-               set(correct_scoops) == set(selected_scoops)
-               ):
-                pass
-            return HttpResponseRedirect(reverse("polls:results", args=(waffel.id,)))
     else:
-        pass
-    
+        raise Http404("Invalid request method")
 
-def get_feedback(selected,correct):
-    feedback = []
-    print(selected)
-    print(correct)
-    for s in selected:
-                if s in correct:
-                    feedback.append({
-                        'sauce':s,
-                        'alert':'success',
-                    })
-                else:
-                    feedback.append({
-                        'sauce':s,
-                        'alert':'danger',
-                    })
-    return feedback
+class WaffelResultView(generic.DetailView):
+    model = Waffel
+    template_name = 'creamsQuizz/waffel_result.html'
+    context_object_name = 'waffel'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Additional context if needed
+        return context
